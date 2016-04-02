9c9fc43cbc1c904d04485bb5ecd73594894459f6
jeffrey <jeffrey.garcia@gmail.com>
2016-04-03 04:17:48 +0800

    fix the getUser webservice request

--- P2pWebServices/src/main/java/edu/polyu/comp/service/UserService.java
+++ P2pWebServices/src/main/java/edu/polyu/comp/service/UserService.java
@@ -7,6 +7,7 @@ import edu.polyu.comp.util.StringUtil;
 public class UserService {
 	
 	private UserDao userDao;
+	
 	public UserDao getUserDao() {
 		if (userDao == null) {
 			userDao = new UserDao();
@@ -14,7 +15,6 @@ public class UserService {
 		return userDao;
 	}
 	
-
 	public boolean createUser(User user) {
 		return getUserDao().createUser(user);
 	}
--- P2pWebServices/src/main/java/edu/polyu/comp/ws/P2pGetUser.java
+++ P2pWebServices/src/main/java/edu/polyu/comp/ws/P2pGetUser.java
@@ -1,30 +1,48 @@
 package edu.polyu.comp.ws;
 
-import java.io.InputStream;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.polyu.comp.domain.User;
 import edu.polyu.comp.service.UserService;
-import edu.polyu.comp.util.StringUtil;
 
 @Path("getUser")
 public class P2pGetUser {
 
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response processInputRequest(InputStream requestBodyStream) {
+//		String output = "";
+//
+//		try {
+//			output = StringUtil.convertInputStreamToString(requestBodyStream);
+//			
+//			// convert the incoming JSON message body into POJO
+//			ObjectMapper jackson = new ObjectMapper();
+//			User user = jackson.readValue(output, User.class);
+//			
+//			// add JDBC call here to retrieve the user name from DBMS using the phone as primary key	
+//			String name = new UserService().findUserNameByPhoneNumber(user.getPhone());
+//			
+//			return Response.status(Response.Status.OK).entity("{\"name\":\"" + name + "\"}").build();
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace(); // log the error
+//			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+//		}
+//	}
+	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response processInputRequest(InputStream requestBodyStream) {
-		String output = "";
-
+	@Path("/{parameter}")
+	public Response processInputRequest(@PathParam("parameter") String parameter) {
+		String output = "{\"phone\":\"" + parameter + "\"}";
+		System.out.println(output);
+		
 		try {
-			output = StringUtil.convertInputStreamToString(requestBodyStream);
-			
 			// convert the incoming JSON message body into POJO
 			ObjectMapper jackson = new ObjectMapper();
 			User user = jackson.readValue(output, User.class);
@@ -39,5 +57,4 @@ public class P2pGetUser {
 			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 		}
 	}
-	
 }
